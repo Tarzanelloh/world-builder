91051c05f6853d711e525138826936b847c8cd49
Tarzanelloh <34843302+Tarzanelloh@users.noreply.github.com>
2017-12-26 00:13:35 +0100

    Create Showandtell.java

---
+++ Showandtell.java
@@ -0,0 +1,17 @@
+package worldbuilder;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Showandtell extends JFrame
+{
+	public Showandtell() 
+	{
+        this.setPreferredSize(new Dimension(500, 1000));
+        this.pack();
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+}
